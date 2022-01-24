265fcce88a8239262d634f06d5884fbd95e51af9
Samir Rolemberg <srolemberg@outlook.com>
2022-01-24 00:04:10 -0300

    virtual keyboard start event

--- lib/widget/virtual_keyboard_widget.dart
+++ lib/widget/virtual_keyboard_widget.dart
@@ -79,45 +79,75 @@ class _VirtualKeyboardWidgetState extends State<VirtualKeyboardWidget>
 
   List<Widget> lineOne() {
     List<Widget> children = [
-      const CharacterBoxWidget(
-        char: "Q",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "W",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "E",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "R",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "T",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "Y",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "U",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "I",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "O",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "P",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
     ];
 
@@ -126,45 +156,75 @@ class _VirtualKeyboardWidgetState extends State<VirtualKeyboardWidget>
 
   List<Widget> lineTwo() {
     List<Widget> children = [
-      const CharacterBoxWidget(
-        char: "A",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "S",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "D",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "F",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "G",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "H",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "J",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "K",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "L",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "Ç",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
     ];
 
@@ -173,33 +233,54 @@ class _VirtualKeyboardWidgetState extends State<VirtualKeyboardWidget>
 
   List<Widget> lineThree() {
     List<Widget> children = [
-      const CharacterBoxWidget(
-        char: "Z",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "X",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "C",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "V",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "B",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "N",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
-      const CharacterBoxWidget(
-        char: "M",
-        status: BoxStatusEnum.empty,
+      const MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: CharacterBoxWidget(
+          char: "Q",
+          status: BoxStatusEnum.empty,
+        ),
       ),
     ];
 
